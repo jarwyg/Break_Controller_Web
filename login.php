104730c7fe118dcb1ff527b166850271697d7023
jarwyg <user@debian-dzwonki-2>
2021-12-29 21:11:57 +0100

    CSS for login page

--- login.php
+++ login.php
@@ -33,12 +33,32 @@ if( isset($_SESSION['login']) ){
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script src="js/bootstrap.bundle.min.js"></script>
 		<script src="js/jquery-3.6.0.min.js"></script>
+
+		<style>
+			.main_div{
+				width: 300px;
+				height: 200px;
+				top: 50%;
+				left: 50%;
+				position: absolute;
+				margin: -100px 0 0 -150px;
+			}
+			
+			.form-signin {
+			  width: 100%;
+			  padding: 15px;
+			  margin: 0 auto;
+			  
+			}
+		</style>
+		
 	</head>
 
 	<body>
-		<div>
-			<form method="POST">
-				<input class="form-control" type="password" name="password">
+		<div class="main_div">
+			<form method="POST" class="form-signin">
+				<div class="text-center h5">Log in</div>
+				<input class="form-control mb-2" type="password" name="password">
 				<input type="submit" class="btn btn-success col-12" value="Zaloguj">
 			</form>
 
