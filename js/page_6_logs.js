46f130bc8b74e8018d906796f43f063ba7454be3
jarwyg <user@debian-dzwonki-2>
2021-12-29 23:20:12 +0100

    Displaying log filenames

--- js/page_6_logs.js
+++ js/page_6_logs.js
@@ -4,15 +4,42 @@ function make_logs_div(data, append_to_element){
 		$('<div></div>')
 			.attr('id', 'logs_div')
 			.addClass('d-none')
-			//.append(make_bells_weekdays_checkboxes(data))
-			.text("logs_div")
+			.append(make_logs_select(data))
+			
 	)
 	
 }
 
 
-//function make_bells_weekdays_checkboxes(data){
-	
-	
-	
-//}
+function make_logs_select(data){
+
+	var select_element = $('<select></select>')
+		.addClass('form-control')
+		.attr('onchange', 'get_log_by_filename(this)')
+
+	select_element.append(
+		new Option("-- Wybierz --", "0")
+	)
+
+	data.log_files.forEach(function(log_files_val){
+		select_element.append(
+			new Option(log_files_val)
+		)
+	})
+
+
+	var div_to_return = $('<div></div>')
+		.append(select_element)
+
+
+	return div_to_return;
+}
+
+
+function get_log_by_filename(element){
+	var filename = $(element).val();
+	if(filename != "0"){
+		console.log( filename )
+	}
+
+}
